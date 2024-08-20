aa5b348772b488b2bbdfb5bf2f7e99b5687246c9
ricardo david <ricardo.david@i3television.es>
2024-08-20 11:15:59 +0200

    actualizacion logica en JSON y excel temporal

--- src/core/types.d.ts
+++ src/core/types.d.ts
@@ -2,7 +2,13 @@ declare type CellData = string | number | boolean;
 declare type JsonData = Record<string, string | number | boolean | null>;
 declare type HeaderRow = string[]; // Header row type
 declare type RowData = CellData[];
-declare type SheetToJsonOutput = [HeaderRow, ...RowData];
+declare type SheetToJsonOutput = [HeaderRow, ...RowData[]];
+
+declare interface GlobalData {
+	excel: { header: HeaderRow; rows: RowData[] };
+	json: string;
+}
+
 declare interface Excel {
 	header: string[];
 	rows: RowData[];
